e018098caef81a885e2241ccc6c6d3344a37fb78
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2020-11-02 16:08:57 -0600

    added styling for about section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
+import {HashRouter} from 'react-router-dom';
+import {HashLink} from 'react-router-hash-link';
+import { BsArrowRight } from "react-icons/bs";
+import {SiRuby, SiReact, SiJavascript, SiRails, SiAdobexd, SiAdobe, SiPostgresql, SiHtml5, SiCss3, SiVisualstudiocode} from "react-icons/si"
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+    <section className="header">
+      <div className="black-overlay">
+        <div className="headline">
+          <h1 className="headline-words">HELLO, MY NAME IS <span className="headline-name">TOMAS</span>
+          <br/>
+          I'M A FULL STACK WEB DEVELOPER</h1>
+          <HashRouter>
+            <HashLink className="route-button" smooth to="#about-section">
+            <button className="hero-button">
+              Read More <BsArrowRight className="hero-button-arrow"/>
+            </button>
+            </HashLink>
+          </HashRouter>
+        </div>
+      </div>
+    </section>
+    <section className="about-section" id="about-section">
+        <div className="skills-div">
+          <ul className="about-skills">
+            <li><SiReact size="3vmax"/></li>
+            <li><SiJavascript size="3vmax"/></li>
+            <li><SiRuby size="3vmax"/></li>
+            <li><SiRails size="3vmax"/></li>
+            <li><SiAdobexd size="3vmax"/></li>
+            <li><SiAdobe size="3vmax"/></li>
+            <li><SiPostgresql size="3vmax"/></li>
+            <li><SiHtml5 size="3vmax"/></li>
+            <li><SiCss3 size="3vmax"/></li>
+            <li><SiVisualstudiocode size="3vmax"/></li>
+          </ul>
+        </div>
+      <div className="section-header">
+        <h1>ABOUT</h1>
+        <div className="break"></div>
+      </div>
+      <div className="about-boxes">
+        <div className="box-item1"></div>
+        <div className="box-item2"></div>
+      </div>
+    </section>
+    </Fragment>
   );
 }
 
