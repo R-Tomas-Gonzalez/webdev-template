309f5cb90962ee30bf833f81d4c8268634857e09
Tomas Gonzalez <r.tomas.gonzalez01@gmail.com>
2022-08-29 23:11:41 -0500

    made hero image smaller on webpage

--- src/hero/hero-image/HeroImage.js
+++ src/hero/hero-image/HeroImage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './heroimage.scss';
-import  Social  from '../social-contact/Social';
+import Social from '../social-contact/Social';
 
 export default function HeroImage() {
     return (
         <div className='image-container'>
-            <img className="hero-image" src='./hero_with_bg.png' />
+            <img className="hero-image" src='./hero_with_bg_smaller.png' alt="Portrait of Tomas Gonzalez" />
             <Social />
         </div>
     )
